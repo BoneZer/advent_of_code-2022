9e99bdb5afa3807749b4c8fb0bb5c059466f30b3
mblex <matthias.blex@googlemail.com>
2022-12-03 11:13:28 +0100

    Add day three

---
+++ days/three/three.go
@@ -0,0 +1,86 @@
+package three
+
+import (
+	"adventofcode/days"
+)
+
+func GetTask() days.Day {
+	return days.Day{
+		Name:             "Two",
+		AbsoluteFilepath: "./days/three/input.txt",
+		Task1:            resolveTaskOne,
+		Task2:            resolveTaskTwo,
+	}
+}
+
+func resolveTaskOne(input []string) int {
+	itemSum := 0
+
+	for _, backpack := range input {
+		comparsements := splitBackpackInput(backpack)
+		doubleItem := findDoubleItem(comparsements)
+		itemSum += getPriorityPoints(doubleItem)
+	}
+
+	return itemSum
+}
+
+func resolveTaskTwo(input []string) int {
+	itemSum := 0
+	groupsOfElves := getGroupsOfElves(input)
+	for _, groupOfElves := range groupsOfElves {
+		badge := getBadgeOfElveGroup(groupOfElves)
+		itemSum += getPriorityPoints(badge)
+	}
+
+	return itemSum
+}
+
+func splitBackpackInput(input string) []string {
+	return []string{input[0 : len(input)/2], input[len(input)/2:]}
+}
+
+func findDoubleItem(input []string) rune {
+	for _, item := range input[0] {
+		for _, secondItem := range input[1] {
+			if item == secondItem {
+				return item
+			}
+		}
+	}
+	return 0
+}
+
+func getPriorityPoints(input rune) int {
+	if input > 96 && input < 123 {
+		return int(input) - 96
+	} else if input > 64 && input < 91 {
+		return int(input) - 38
+	}
+	return 0
+}
+
+func getGroupsOfElves(input []string) [][]string {
+	groupOfElves := [][]string{}
+
+	for i := 0; i < len(input)-2; i += 3 {
+		group := []string{input[i], input[i+1], input[i+2]}
+		groupOfElves = append(groupOfElves, group)
+	}
+
+	return groupOfElves
+}
+
+func getBadgeOfElveGroup(input []string) rune {
+	for _, firstElf := range input[0] {
+		for _, secondElf := range input[1] {
+			for _, thirdElf := range input[2] {
+				if firstElf == secondElf && secondElf == thirdElf {
+					return firstElf
+				}
+			}
+		}
+	}
+
+	return 0
+}
---
+++ days/three/three_test.go
@@ -0,0 +1,270 @@
+package three
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func getTestArray() []string {
+	readFile, err := os.Open("./test.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	stringArray := []string{}
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+	for fileScanner.Scan() {
+		stringArray = append(stringArray, fileScanner.Text())
+	}
+	return stringArray
+}
+
+func TestResolveTaskOne(t *testing.T) {
+	type args struct {
+		input []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Test input",
+			args: args{
+				input: getTestArray(),
+			},
+			want: 157,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := resolveTaskOne(tt.args.input); got != tt.want {
+				t.Errorf("resolveTaskOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestResolveTaskTwo(t *testing.T) {
+	type args struct {
+		input []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Test input",
+			args: args{
+				input: getTestArray(),
+			},
+			want: 70,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := resolveTaskTwo(tt.args.input); got != tt.want {
+				t.Errorf("resolveTaskTwo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_splitBackpackInput(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "First Test input",
+			args: args{
+				input: "vJrwpWtwJgWrhcsFMMfFFhFp",
+			},
+			want: []string{
+				"vJrwpWtwJgWr", "hcsFMMfFFhFp",
+			},
+		},
+
+		{
+			name: "Second Test input",
+			args: args{
+				input: "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+			},
+			want: []string{"jqHRNqRjqzjGDLGL", "rsFMfFZSrLrFZsSL"},
+		}, {
+			name: "Third Test input",
+			args: args{
+				input: "PmmdzqPrVvPwwTWBwg",
+			},
+			want: []string{"PmmdzqPrV", "vPwwTWBwg"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := splitBackpackInput(tt.args.input); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("splitBackpackInput() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_findDoubleItem(t *testing.T) {
+	type args struct {
+		input []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want rune
+	}{
+		{
+			name: "First Test input",
+			args: args{
+				input: []string{
+					"vJrwpWtwJgWr", "hcsFMMfFFhFp",
+				},
+			},
+			want: 'p',
+		},
+
+		{
+			name: "Second Test input",
+			args: args{
+				input: []string{"jqHRNqRjqzjGDLGL", "rsFMfFZSrLrFZsSL"},
+			},
+			want: 'L',
+		}, {
+			name: "Third Test input",
+			args: args{
+				input: []string{"PmmdzqPrV", "vPwwTWBwg"},
+			},
+			want: 'P',
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findDoubleItem(tt.args.input); got != tt.want {
+				t.Errorf("findDoubleItem() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getPriorityPoints(t *testing.T) {
+	type args struct {
+		input rune
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "First test input",
+			args: args{input: 'p'},
+			want: 16,
+		},
+		{
+			name: "Second test input",
+			args: args{input: 'L'},
+			want: 38,
+		},
+		{
+			name: "Third test input",
+			args: args{input: 'P'},
+			want: 42,
+		},
+		{
+			name: "Fourth test input",
+			args: args{input: 'v'},
+			want: 22,
+		},
+		{
+			name: "Fifth test input",
+			args: args{input: 't'},
+			want: 20,
+		},
+		{
+			name: "Sixth test input",
+			args: args{input: 's'},
+			want: 19,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getPriorityPoints(tt.args.input); got != tt.want {
+				t.Errorf("getPriorityPoints() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getGroupsOfElves(t *testing.T) {
+	type args struct {
+		input []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]string
+	}{
+		{
+			name: "Test input",
+			args: args{
+				[]string{"vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg", "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw"},
+			},
+			want: [][]string{
+				{"vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg"}, {"wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getGroupsOfElves(tt.args.input); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getGroupsOfElves() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getBadgeOfElveGroup(t *testing.T) {
+	type args struct {
+		input []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want rune
+	}{
+		{
+			name: "First Test input",
+			args: args{
+				input: []string{"vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg"},
+			},
+			want: 'r',
+		},
+		{
+			name: "Second Test input",
+			args: args{
+				input: []string{"wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw"},
+			},
+			want: 'Z',
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getBadgeOfElveGroup(tt.args.input); got != tt.want {
+				t.Errorf("getBadgeOfElveGroup() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
